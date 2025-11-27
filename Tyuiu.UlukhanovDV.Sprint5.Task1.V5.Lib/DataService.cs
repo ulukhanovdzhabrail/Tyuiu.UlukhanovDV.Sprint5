2e06f374193856c43f6e4e986a05ef941fa13b1e
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-11-27 17:41:26 +0500

    Task1

---
+++ Tyuiu.UlukhanovDV.Sprint5.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.UlukhanovDV.Sprint5.Task1.V5.Lib
+{
+    public class DataService : ISprint5Task1V5
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = 5 - 3 * x + (1 + Math.Sin(x))/(2*x - 0.5);
+                y = Math.Round(y, 2);
+                strY = Convert.ToString(y);
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.UlukhanovDV.Sprint5.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.UlukhanovDV.Sprint5.Task1.V5.Lib;
+namespace Tyuiu.UlukhanovDV.Sprint5.Task1.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = @"C:\Users\djabr\AppData\Local\Temp\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(s);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
